721b4c8b8f86b78424a1e69fd036d8f5f9aa359a
Yan Aung Hein <yan@apmhelp.com>
2025-09-07 01:14:59 +0700

    update og image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 
 <svelte:head>
 	<title>La Persona</title>
+
 	<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
 	<link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
@@ -22,6 +23,30 @@
 	<meta name="msapplication-TileColor" content="#121212" />
 	<meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
 	<meta name="theme-color" content="#121212" />
+
+	<!-- Open Graph -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="La Persona" />
+	<!-- <meta
+		property="og:description"
+		content="La Persona is a platform that allows you to create and share your digital business card with your friends and family."
+	/> -->
+	<meta property="og:url" content="https://imgsrc.io/guides/open-graph-meta-tags" />
+
+	<meta property="og:image" content="/og.png" />
+	<meta property="og:image:width" content="1050" />
+	<meta property="og:image:height" content="600" />
+
+	<!-- Twitter/X -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="La Persona" />
+	<!-- <meta
+		name="twitter:description"
+		content="La Persona is a platform that allows you to create and share your digital business card with your friends and family."
+	/> -->
+	<meta name="twitter:image" content="/og.png" />
+	<meta name="twitter:image:width" content="1050" />
+	<meta name="twitter:image:height" content="600" />
 </svelte:head>
 
 {@render children()}
