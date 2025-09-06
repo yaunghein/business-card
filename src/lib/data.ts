8804071687eb3afd60e85e8c63f5f11c2c9362d6
Yan Aung Hein <yan@apmhelp.com>
2025-09-07 01:01:02 +0700

    update menu ui

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,45 +1,137 @@
 export default [
 	{
 		id: 'maythazinmoenyunt@urbanus',
+		name: 'Maythazin Moenyunt',
 		spline: 'https://my.spline.design/untitled-daaf8707e731320f7ac1627373f3221c/',
 		vcf: '/vcfs/mtmn.vcf',
 		phone: '+9592055594',
-		website: 'https://www.tiktok.com/@urbanus0209'
+		links: [
+			{
+				icon: 'world',
+				href: 'https://www.tiktok.com/@urbanus0209',
+				label: 'Go to Website'
+			},
+			{
+				icon: 'arrowDown',
+				href: 'https://www.tiktok.com/@urbanus0209',
+				label: 'Download Resume'
+			},
+			{
+				icon: '/images/facebook.png',
+				href: 'https://www.tiktok.com/@urbanus0209',
+				label: 'Facebook'
+			},
+			{
+				icon: '/images/instagram.png',
+				href: 'https://www.tiktok.com/@urbanus0209',
+				label: 'Instagram'
+			},
+			{
+				icon: '/images/linkedin.png',
+				href: 'https://www.tiktok.com/@urbanus0209',
+				label: 'LinkedIn'
+			}
+		]
 	},
 	{
 		id: 'minhtetdipar@thesandstudio',
+		name: 'Minhtet Di Par',
 		spline: 'https://my.spline.design/kodipardigitalbusinesscard-5b7ed0859b13202dceb80e7995b0266d/',
 		vcf: '/vcfs/minhtetdipar.vcf',
 		phone: '+65 87685983',
-		website: 'https://thesandstudio.com/'
+		links: [
+			{
+				icon: 'world',
+				href: 'https://thesandstudio.com/',
+				label: 'Go to Website'
+			}
+		]
 	},
 	{
 		id: 'hannah',
+		name: 'Hannah',
 		spline: 'https://my.spline.design/hannahdigitalbusinesscard-4f2c2cf66e0ca057458df2088587a931/',
 		vcf: '/vcfs/minhtetdipar.vcf',
 		phone: '+65 87685983',
-		website: ''
+		links: [
+			{
+				icon: 'world',
+				href: 'https://hannah.com/',
+				label: 'Go to Website'
+			}
+		]
 	},
 	{
 		id: 'hlayuparaye@digiboxes',
+		name: 'Hla Yupar Aye',
 		spline: 'https://my.spline.design/hlaayedigitalbusinesscard-0f8d94c20d7a457552d4c7becf417711/',
 		vcf: '/vcfs/hlayuparaye.vcf',
 		phone: '09775097767',
-		website: 'https://digiboxes.com.mm/'
+		links: [
+			{
+				icon: 'world',
+				href: 'https://digiboxes.com.mm/',
+				label: 'Go to Website'
+			}
+		]
 	},
 	{
 		id: 'aungpyaekyaw',
+		name: 'Aung Pyae Kyaw',
 		spline:
 			'https://my.spline.design/aungpyaedigitalbusinesscard-7a4462430bd14bd687b2039df05de743/',
 		vcf: '/vcfs/apk.vcf',
 		phone: '0626365372',
-		website: 'https://www.la-persona.com/'
+		links: [
+			{
+				icon: 'world',
+				href: 'https://www.duodesignlab.com/about/aung-pyae-kyaw',
+				label: 'Go to Website'
+			},
+			{
+				icon: '/images/facebook.png',
+				href: 'https://www.facebook.com/aung.pyae.406337/',
+				label: 'Facebook'
+			},
+			{
+				icon: '/images/instagram.png',
+				href: 'https://www.instagram.com/kyaw.aungpyae/',
+				label: 'Instagram'
+			},
+			{
+				icon: '/images/linkedin.png',
+				href: 'https://www.linkedin.com/in/aung-pyae-kyaw-2346aa16b/',
+				label: 'LinkedIn'
+			}
+		]
 	},
 	{
 		id: 'yanaunghein',
+		name: 'Yan Aung Hein',
 		spline: 'https://my.spline.design/yanaungdigitalbusinesscard-277b9e5b63017458131b2ddd22407e38/',
 		vcf: '/vcfs/yanaunghein.vcf',
 		phone: '0626366748',
-		website: 'https://www.la-persona.com/'
+		links: [
+			{
+				icon: 'world',
+				href: 'https://www.duodesignlab.com/about/yan-aung-hein',
+				label: 'Go to Website'
+			},
+			{
+				icon: '/images/facebook.png',
+				href: 'https://www.facebook.com/yaunghein311/',
+				label: 'Facebook'
+			},
+			{
+				icon: '/images/instagram.png',
+				href: 'https://www.instagram.com/yuzu.americano/',
+				label: 'Instagram'
+			},
+			{
+				icon: '/images/linkedin.png',
+				href: 'https://www.linkedin.com/in/yanaunghein/',
+				label: 'LinkedIn'
+			}
+		]
 	}
 ];
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,25 @@
-<script>
+<script lang="ts">
+	import Menu from '$lib/components/icons/menu.svelte';
+	import Phone from '$lib/components/icons/phone.svelte';
+	import Close from '$lib/components/icons/close.svelte';
+	import World from '$lib/components/icons/world.svelte';
+	import ArrowDown from '$lib/components/icons/arrow-down.svelte';
+
 	let { data } = $props();
+
+	let isMenuOpen = $state(false);
+
+	const iconMap = {
+		world: World,
+		arrowDown: ArrowDown
+	} as const;
 </script>
 
-<div class="relative z-10 h-[100svh] w-screen">
+<svelte:head>
+	<title>{data.person.name} | La Persona</title>
+</svelte:head>
+
+<div class="relative z-10 h-[100svh] w-screen overflow-hidden">
 	<!-- Spline Scene -->
 	<iframe
 		title="Spline Scene"
@@ -11,49 +28,99 @@
 		frameborder="0"
 		allow="autoplay"
 	></iframe>
-	<!-- Contents -->
-	<div class="absolute bottom-8 left-0 flex w-full flex-row justify-center gap-2 px-4">
-		<!-- Phone -->
-		<a aria-label="phone" href="tel:{data.person.phone}">
-			<div
-				class="flex h-16 w-16 items-center justify-center rounded-full border border-neutral-900 active:border-neutral-100"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24px"
-					viewBox="0 -960 960 960"
-					width="24px"
-					fill="#e8eaed"
-					><path
-						d="M777.61-140q-113.07 0-227.19-52.58-114.11-52.58-209.88-148.34-95.38-95.77-147.96-209.7Q140-664.54 140-777.61q0-18 12-30.2Q164-820 182-820h130.46q15.15 0 26.73 9.89 11.58 9.88 14.73 24.42L376.85-668q2.38 16.38-1 28.15-3.39 11.77-12.16 19.77l-92.38 89.93q22.3 40.84 50.96 77.27 28.65 36.42 62.04 69.57 32.92 32.93 70 61.16 37.08 28.23 80.08 52.54l89.76-90.54q9.39-9.77 22.74-13.7 13.34-3.92 27.73-1.92l111.07 22.62q15.15 4 24.73 15.46t9.58 26V-182q0 18-12.19 30-12.2 12-30.2 12Z"
-					/></svg
-				>
-			</div>
-		</a>
-		<!-- Website -->
-		<a aria-label="website" href={data.person.website}>
-			<div
-				class="flex h-16 w-16 items-center justify-center rounded-full border border-neutral-900 active:border-neutral-100"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24px"
-					viewBox="0 -960 960 960"
-					width="24px"
-					fill="#e8eaed"
-					><path
-						d="M480-100q-78.15 0-147.5-29.96t-120.96-81.58q-51.62-51.61-81.58-120.96T100-480q0-78.77 29.96-147.81t81.58-120.65q51.61-51.62 120.96-81.58T480-860q78.77 0 147.81 29.96t120.65 81.58q51.62 51.61 81.58 120.65T860-480q0 78.15-29.96 147.5t-81.58 120.96q-51.61 51.62-120.65 81.58T480-100Zm0-60.85q30.62-40.61 51.54-81.92 20.92-41.31 34.08-90.31H394.38q13.93 50.54 34.47 91.85 20.53 41.31 51.15 80.38Zm-77.46-11q-23-33-41.31-75.03-18.31-42.04-28.46-86.2H197.08q31.69 62.31 85 104.7 53.31 42.38 120.46 56.53Zm154.92 0q67.15-14.15 120.46-56.53 53.31-42.39 85-104.7H627.23q-12.08 44.54-30.39 86.58-18.3 42.04-39.38 74.65ZM171.92-393.08h148.7q-3.77-22.3-5.47-43.73-1.69-21.42-1.69-43.19 0-21.77 1.69-43.19 1.7-21.43 5.47-43.73h-148.7q-5.77 20.38-8.84 42.38-3.08 22-3.08 44.54t3.08 44.54q3.07 22 8.84 42.38Zm208.69 0h198.78q3.76-22.3 5.46-43.34 1.69-21.04 1.69-43.58t-1.69-43.58q-1.7-21.04-5.46-43.34H380.61q-3.76 22.3-5.46 43.34-1.69 21.04-1.69 43.58t1.69 43.58q1.7 21.04 5.46 43.34Zm258.77 0h148.7q5.77-20.38 8.84-42.38 3.08-22 3.08-44.54t-3.08-44.54q-3.07-22-8.84-42.38h-148.7q3.77 22.3 5.47 43.73 1.69 21.42 1.69 43.19 0 21.77-1.69 43.19-1.7 21.43-5.47 43.73Zm-12.15-233.84h135.69Q730.85-690 678.5-731.62q-52.35-41.61-121.04-56.92 23 34.92 40.92 76.39 17.93 41.46 28.85 85.23Zm-232.85 0h171.24q-13.93-50.16-35.04-92.43-21.12-42.27-50.58-79.8-29.46 37.53-50.58 79.8-21.11 42.27-35.04 92.43Zm-197.3 0h135.69q10.92-43.77 28.85-85.23 17.92-41.47 40.92-76.39-69.08 15.31-121.23 57.12-52.16 41.8-84.23 104.5Z"
-					/>
-				</svg>
-			</div>
-		</a>
-		<!-- Save Contact -->
+	<div class="absolute bottom-8 left-0 right-0 flex w-full gap-3 px-5 sm:mx-auto sm:max-w-96">
 		<a
 			href={data.person.vcf}
 			download="{data.person.id}.vcf"
-			class="text-md flex flex-grow items-center justify-center rounded-full bg-neutral-900 px-3 py-2 text-center font-light text-white active:bg-neutral-100 active:text-black sm:hidden"
+			class="grid flex-1 place-items-center rounded-full border border-white/10 bg-white/10 text-sm font-bold"
 		>
 			Save Contact
 		</a>
+		<button
+			onclick={() => (isMenuOpen = !isMenuOpen)}
+			class="grid aspect-square w-[3.25rem] shrink-0 place-items-center rounded-full border border-white/10 bg-white/10"
+		>
+			<div class="aspect-square w-6">
+				<Menu />
+			</div>
+		</button>
+		<!-- <a
+			aria-label="phone"
+			href="tel:{data.person.phone}"
+			class="grid aspect-square w-[3.25rem] shrink-0 place-items-center rounded-full border border-white/10 bg-white/10"
+		>
+			<div class="aspect-square w-6">
+				<Phone />
+			</div>
+		</a> -->
+	</div>
+
+	<div
+		class="fixed inset-0 top-auto -mb-[1px] w-full scale-[1.005] rounded-t-xl border border-white/10 bg-[#121212] py-6 transition duration-[750ms] sm:mx-auto sm:max-w-96 {isMenuOpen
+			? 'translate-y-0'
+			: 'translate-y-full'}"
+	>
+		<div>
+			<button
+				onclick={() => (isMenuOpen = false)}
+				class="absolute right-4 top-4 grid aspect-square w-8 place-items-center rounded-full border border-white/10 bg-white/10 transition duration-[750ms] {isMenuOpen
+					? 'opacity-100'
+					: 'opacity-0'}"
+			>
+				<div class="aspect-square w-[0.62rem]">
+					<Close />
+				</div>
+			</button>
+			<div
+				class="text-center text-sm font-bold transition duration-[750ms] {isMenuOpen
+					? 'opacity-100'
+					: 'opacity-0'}"
+			>
+				Explore
+			</div>
+			<div class="hide-scrollbar mt-8 flex gap-3 overflow-x-scroll px-5">
+				{#each data.person.links as link, index}
+					{@const isImage = link.icon.includes('.')}
+					{#if isImage}
+						<a
+							href={link.href}
+							class="flex shrink-0 flex-col items-center gap-4 transition duration-[750ms] {isMenuOpen
+								? 'opacity-100'
+								: 'opacity-0'}"
+							style="transition-delay: {(index + 1) * 100}ms"
+						>
+							<div class="grid aspect-square w-[4.56rem] overflow-hidden rounded-full bg-white/10">
+								<img
+									src={link.icon}
+									alt={link.label}
+									class="aspect-square h-full w-full object-cover"
+								/>
+							</div>
+							<div class="max-w-[4rem] text-center text-xs leading-[1.1]">{link.label}</div>
+						</a>
+					{:else}
+						{@const IconComponent = iconMap[link.icon as keyof typeof iconMap]}
+						<a
+							href={link.href}
+							class="flex shrink-0 flex-col items-center gap-4 transition duration-[750ms] {isMenuOpen
+								? 'opacity-100'
+								: 'opacity-0'}"
+							style="transition-delay: {(index + 1) * 100}ms"
+						>
+							<div
+								class="grid aspect-square w-[4.56rem] place-items-center rounded-full bg-white/10"
+							>
+								<div class="aspect-square w-8">
+									{#if IconComponent}
+										<IconComponent />
+									{/if}
+								</div>
+							</div>
+							<div class="max-w-[4rem] text-center text-xs leading-[1.1]">{link.label}</div>
+						</a>
+					{/if}
+				{/each}
+			</div>
+		</div>
 	</div>
 </div>
