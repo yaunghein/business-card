0d37d609ee85ce405b9dd82caa103d51531708c1
Yan Aung Hein <yan@apmhelp.com>
2025-12-02 03:48:55 +0700

    initial fci

---
+++ src/routes/founders-club-invitation/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// import { onMount } from 'svelte';
+
+	// onMount(() => {
+	// 	document.addEventListener('click', () => {
+	// 		console.log('clcil');
+	// 	});
+	// });
+</script>
+
+<div class="relative z-10 h-[100dvh] w-screen overflow-hidden bg-white">
+	<iframe
+		title="Spline Scene"
+		src="https://my.spline.design/foundersclubscene-RFo2rRC47y3NH7PCjmHFlaj6/"
+		class="absolute left-0 top-0 z-0 h-full w-full"
+		frameborder="0"
+		allow="autoplay"
+	></iframe>
+</div>
